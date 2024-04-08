67e0fc15d12f56fb51d4bc0a551e9aa425aa9fe9
Debasish Arup Sahu <162704473+debasish414@users.noreply.github.com>
2024-04-08 22:06:02 +0530

    Update script.js

--- Fertilizer web templete/script.js
+++ Fertilizer web templete/script.js
@@ -1,1 +1,39 @@
+const star = document.querySelectorAll(".star")
 
+let selectrdStar = -1;
+star.forEach((item, index) => {
+    // console.log(item);
+    item.dataset.rating = index + 1
+    item.addEventListener('mouseover', handlemouseover)
+    item.addEventListener('click', handleclick)
+    item.addEventListener('mouseleave', handlemouseleave)
+});
+
+function handlemouseover(e) {
+    // console.log(e.target.dataset.rating);
+    let output = e.target.dataset.rating;
+    if (!output) return;
+    else handleUpdate(output);
+};
+
+function handleUpdate(getCurrentValue) {
+    for (let i = 0; i < 5; i++) {
+        if (i < getCurrentValue) {
+            star[i].classList.replace('star', 'star-u')
+        } else {
+            star[i].classList.replace('star-u', 'star')
+        }
+    }
+};
+
+function handleclick(e) {
+    // console.log(e.target.dataset.rating);
+    let output = e.target.dataset.rating;
+    selectrdStar = output
+    handleUpdate(selectrdStar)
+
+};
+function handlemouseleave(e) {
+    // console.log(e.target.dataset.rating);
+    handleUpdate(selectrdStar)
+};
